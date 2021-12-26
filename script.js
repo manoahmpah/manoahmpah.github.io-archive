a7b66290ad7ea2a15eed72e5ebf0399bfc6d6b99
Manoah <88939578+manoahmpah@users.noreply.github.com>
2021-12-26 23:56:25 +0100

    New portFolio

---
+++ script.js
@@ -0,0 +1,17 @@
+// let controller = new ScrollMagic.Controller();
+// let timeline = new TimelineMax();
+
+// timeline
+// // .to('#image_front', 3 , {top:'0%'}, "-=3 ")
+// .fromTo('#Le_titre', { opacity: 0 }, {opacity: 1, duration: 3 } )
+
+
+// let scene = new ScrollMagic.Scene({
+//     triggerElement: ".yo",
+//     duration: "300%",
+//     triggerHook: 0,
+//   })
+
+//     .setTween(timeline)
+//     .setPin("section")
+//     .addTo(controller);
